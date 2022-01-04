47136e8bbdd4aac50679f2b04582f94bf01ff317
Naveen Kadiyala <venkatnaveen418@gmail.com>
2022-01-04 17:23:31 +0530

    Resolved the db connection issue for logging

--- startup/logging.js
+++ startup/logging.js
@@ -26,7 +26,7 @@ module.exports = function () {
     winston.add(new winston.transports.File({ filename: 'logfile.log' }))
     winston.add(new winston.transports.Console())
     winston.add(new winston.transports.MongoDB({
-        db: 'mongodb://localhost/vidly',
+        db: config.get('db'),
         options: { useUnifiedTopology: true },
         collection: 'server_logs'
     }))
